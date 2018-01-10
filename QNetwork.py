8192d0b8e175013dd7edf32aa9927761067219fc
David Ariel <davidariel333@gmail.com>
2018-01-10 14:34:05 +0100

    readme added finished the network, now building the training part

--- QNetwork.py
+++ QNetwork.py
@@ -1,7 +1,8 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import os
+import scipy.signal
 
 ALPHABET_SIZE = 4
 CONV_1_KERNEL = 3
@@ -11,63 +12,81 @@ CONV_2_DEPTH = 50
 AFFINE_1_WIDTH = 100
 AFFINE_2_WIDTH = 50
 
-A = 1
-ACTION_LAYER_1_FACTOR = 2
-ACTION_LAYER_2_FACTOR = 1
-MERGED_LAYER_FATOR_1 = 1.1
-MERGED_LAYER_FATOR_2 = 1.1
+
 
 
 class QNetwork(object):
     def __init__(self, sequence_length):
         print('Building Q network')
-        self.total_value = 0
         self.LEARNING_RATE = 0.00001
-        self.BETA_W = 10 ** -3
-        self.weights = []
+        self.VALUE_LOSS_FACTOR = 0.5
+        self.POLICY_LOSS_FACTOR = 0.5
+        self.ENTROPY_LOSS_FACTOR = 0.01
         self.sequence_length = sequence_length
-        # ? x SIG x L
-        self.seq1hot_shape = [None, ALPHABET_SIZE, self.sequence_length]
-        # ? x L x L
-        self.seq_eng_mat_shape = [None, self.sequence_length, self.sequence_length, (ALPHABET_SIZE ** 2) + 1]
         self._init_inputs()
         # self._init_action_layers()
         self._init_general_state_layer()
         self._init_hidden_layers()
-        self._init_outputs()
+        self._init_output_layer()
+        self._init_loss()
 
-    def _init_outputs(self):
-        # advantages = rewards + gamma * self.value_plus[1:] - self.value_plus[:-1]
-        # Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
-        self.target_value = tf.placeholder(shape=[None], dtype=tf.float32, name='target_value')
-        self.advantages = tf.placeholder(shape=[None], dtype=tf.float32, name='advantages')
+    def _init_loss(self):
+        self.action_onehot = tf.one_hot(self.action_input, self.sequence_length*ALPHABET_SIZE, dtype=tf.float32)
+        self.responsible_outputs = tf.reduce_sum(self.policy * self.action_onehot, [1])
+
+        # self.indexes = tf.range(0, tf.shape(self.policy)[0]) * tf.shape(self.policy)[1] + self.action_input
+        # self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes)
+
+        rewards_plus = np.asarray(self.reward.tolist() + [self.BOOTSTRAP_VALUE])
+        self.discounted_rewards = discout(rewards_plus, self.DISCOUNT_RATE)[:-1]
+        self.values_plus = np.asarray(self.values.tolist() + [self.BOOTSTRAP_VALUE])
+        marginal_advantages = self.reward + self.DISCOUNT_RATE * self.values_plus[1:] - self.values_plus[:-1]
+        self.advantages = discount(marginal_advantages, self.DISCOUNT_RATE)
+
+        # self.policy_loss = -tf.reduce_mean(tf.log(self.responsible_outputs)*self.discounted_rewards)
+
         with tf.name_scope('error'):
-            value_loss = tf.reduce_sum(tf.square(self.target_value - self.value))
-            policy_loss = -tf.reduce_sum(tf.log(self.q_est)*self.advantages)
-            entropy = -tf.reduce_sum(self.policy * tf.log(self.policy))
-            print(entropy.shape)
-            input("4")
-            with tf.name_scope('regularization'):
-                self.regularizers = sum([tf.nn.l2_loss(weight) for weight in self.weights])
+            self.value_loss = tf.reduce_sum(tf.square(self.discounted_rewards - self.value))
+            self.policy_loss = -tf.reduce_sum(tf.log(self.responsible_outputs)*self.advantages)
+            self.entropy = -tf.reduce_sum(self.policy * tf.log(self.policy))
+            self.loss = self.VALUE_LOSS_FACTOR * self.value_loss +
+                        self.POLICY_LOSS_FACTOR * self.policy_loss -
+                        self.ENTROPY_LOSS_FACTOR * self.entropy
+    def _init_trainger(self):
+        trainer = tf.train.GradientDescentOptimizer(learning_rate=self.LEARNING_RATE)
+        self.updateModel = trainer.minimize(self.loss)
 
-        with tf.name_scope('loss'):
-            self.loss = tf.reduce_mean(value_loss + policy_loss + self.BETA_W * self.regularizers + entropy)
-            tf.summary.scalar('total_loss', self.loss)
-            tf.summary.scalar('value_loss', value_loss)
-            tf.summary.scalar('policy_loss', policy_loss)
-            tf.summary.scalar('regularizer', self.regularizers)
+    def _init_summaries(self):
+            tf.summary.histogram('total_loss', self.loss)
+            tf.summary.histogram('value_loss', self.value_loss)
+            tf.summary.histogram('policy_loss', self.policy_loss)
+            tf.summary.histogram('entropy', self.entropy)
             tf.summary.histogram('advantages', self.advantages)
-            tf.summary.histogram('target_value', self.target_value)
+            tf.summary.histogram('discounted_rewards', self.discounted_rewards)
+            tf.summary.histogram('policy', self.policy)
+            tf.summary.histogram('value', self.value)
 
-        trainer = tf.train.AdamOptimizer(learning_rate=self.LEARNING_RATE)
-        self.update_model = trainer.minimize(self.loss)
 
-    def train(self, discounted_rewards, states, actions, advantages, sess):
-        feed_dict = self.states_to_feed_dict(states)
-        feed_dict[self.target_value] = discounted_rewards
+    def _init_output_layer(self):
+        self.policy = self._gen_affine_layer(self.affine2, , "policy", tf.nn.softmax)
+        self.chosen_action = tf.argmax(self.policy, 1)
+        self.value = self._gen_affine_layer(self.affine2, 1, "value", None)
+
+
+    # def _init_outputs(self):
+    #     # advantages = reward + self.DISCOUNT_RATE * self.values_plus[1:] - self.values_plus[:-1]
+    #     # Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+    #         with tf.name_scope('regularization'):
+    #             self.regularizers = sum([tf.nn.l2_loss(weight) for weight in self.weights])
+
+
+
+    def train(self, rewards, states, actions, sess):
+        feed_dict = {}
+        feed_dict[self.state_input] = states
         feed_dict[self.action_input] = actions
-        feed_dict[self.advantages] = advantages
-        sess.run(self.update_model, feed_dict=feed_dict)
+        feed_dict[self.rewards_plus] = rewards
+        sess.run(self.update_model,actions=feed_dict)
         return feed_dict
 
     def get_regularizer(self, sess):
@@ -99,10 +118,11 @@ class QNetwork(object):
 
 
     def _init_inputs(self):
-        with tf.name_scope('input_action'):
-            self.action_input = tf.placeholder(shape=self.seq1hot_shape, dtype=tf.int8, name="action_input")
-        with tf.name_scope('input_state'):
-            self.state_input = tf.placeholder(shape=self.seq_eng_mat_shape, dtype=tf.float32, name="input_state")
+        self.action_input = tf.placeholder(shape=[None], dtype=tf.int32, name="input_action")
+        seq_eng_mat_shape = [None, self.sequence_length, self.sequence_length, (ALPHABET_SIZE ** 2) + 1]
+        self.state_input = tf.placeholder(shape=seq_eng_mat_shape, dtype=tf.float32, name="input_state")
+        self.reward_input = tf.placeholder(shape=[None], dtype=tf.float32, name="input_reward")
+
 
     def _init_hidden_layers(self):
         self.conv1 = self._gen_conv2d_layer(self.state_input, CONV_1_DEPTH, CONV_1_KERNEL, "Conv 1")
@@ -112,23 +132,6 @@ class QNetwork(object):
         conv2_flat = tf.contrib.layers.flatten(self.conv2)
         self.affine1 = self._gen_affine_layer(self.conv2_flat, AFFINE_1_WIDTH, "Affine 1")
         self.affine2 = self._gen_affine_layer(self.affine1, AFFINE_2_WIDTH, "Affine 2")
-
-    def _init_out_layer(self):
-        policy_flat = self._gen_affine_layer(self.affine2, self.sequence_length*ALPHABET_SIZE, "policy_flat", false)
-        ###############
-        policy_raw = tf.reshape(policy_flat, [-1, self.action_size, len(ACTION_VALUES)])
-        self.policy = tf.nn.softmax(policy_raw, dim=2, name='policy_softmax')
-        shifted_actions = self.action_input + tf.constant(1, dtype=tf.int8)
-        shifted_actions = tf.cast(shifted_actions, tf.uint8)
-        onehot_action_input = tf.one_hot(indices=shifted_actions, depth=len(ACTION_VALUES), on_value=1.0, off_value=0.0, axis=2, dtype=tf.float32,)
-        self.q_est = tf.reduce_sum(self.policy * onehot_action_input, axis=[1,2], name='q_est')
-
-        # tf.summary.image('q_est', self.q_est)
-        # tf.summary.image('onehot_action_input', onehot_action_input)
-        # policy = tf.argmax(self.policy, axis=2, name='policy_max')
-        # self.output_action = policy - tf.constant(1, dtype=tf.int64)
-        self.value = self._gen_affine_layer(mixed2, in_size, 1, 'value', use_relu=False    )
-
 
     def _variable_summaries(self, var, name):
         """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
@@ -142,8 +145,7 @@ class QNetwork(object):
             tf.summary.scalar('min', tf.reduce_min(var))
             tf.summary.histogram('histogram', var)
 
-    def _gen_affine_layer(self, input_layer, out_size, layer_name, use_relu=True):
-        activation = tf.nn.relu if use_relu else None
+    def _gen_affine_layer(self, input_layer, out_size, layer_name, activation=tf.nn.relu):
         layer = tf.layers.dense(features, out_size, activation=activation, name=layer_name)
         layer_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, layer_name)
         self._variable_summaries(layer_vars[0], 'weights')
@@ -165,3 +167,7 @@ class QNetwork(object):
         self._variable_summaries(layer_vars[1], 'bias')
         self._variable_summaries(layer, 'act')
         return layer
+
+
+def discount(x, self.DISCOUNT_RATE):
+    return scipy.signal.lfilter([1], [1, -self.DISCOUNT_RATE], x[::-1], axis=0)[::-1]
